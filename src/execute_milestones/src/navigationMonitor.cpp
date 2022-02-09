0bd43dfe526a50f19002afac068d130e67f560fb
Hotchnerz <radam.troy@gmail.com>
2022-02-09 00:37:49 -0500

    Add action client to send a goal to move_base

--- src/execute_milestones/src/navigationMonitor.cpp
+++ src/execute_milestones/src/navigationMonitor.cpp
@@ -2,17 +2,20 @@
 #include <actionlib/client/simple_action_client.h>
 #include <frontier_exploration/ExploreTaskActionResult.h>
 #include <frontier_exploration/ExploreTaskAction.h>
+#include <move_base_msgs/MoveBaseAction.h>
+
+int status = 0;
 
 void exploreStatusCallback(const frontier_exploration::ExploreTaskActionResult::ConstPtr& msg)
 {
-  int status = msg->status.status;
+  status = msg->status.status;
 
-  if (status == 3) {
+  /*if (status == 3) {
   ROS_INFO("Exploration Complete. Returning to start position!");
 
   } else {
       ROS_INFO("Navigation Aborted. Error Occured");
-  }
+  }*/
 }
 
 int main (int argc, char **argv)
@@ -23,10 +26,12 @@ int main (int argc, char **argv)
   // create the action client
   // true causes the client to spin its own thread
   actionlib::SimpleActionClient<frontier_exploration::ExploreTaskAction> unboundEx("explore_server", true);
+  actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> returnHome("move_base", true);
 
   ROS_INFO("Waiting for action server to start.");
   // wait for the action server to start
   unboundEx.waitForServer(); //will wait for infinite time
+  returnHome.waitForServer();
 
   ROS_INFO("Action server started, sending goal.");
   // send a goal to the action
@@ -40,6 +45,23 @@ int main (int argc, char **argv)
   frontierGoal.explore_center.point.z = 0;
 
   unboundEx.sendGoal(frontierGoal);
+
+  move_base_msgs::MoveBaseGoal homeGoal;
+
+  if (status == 3) {
+  ROS_INFO("Exploration Complete. Returning to start position!");
+  homeGoal.target_pose.header.frame_id = "map";
+
+  homeGoal.target_pose.pose.position.x = 0;
+  homeGoal.target_pose.pose.position.y = 0;
+
+  homeGoal.target_pose.pose.orientation.w = 1;
+
+  returnHome.sendGoal(homeGoal);
+
+  } else {
+      ROS_INFO("Navigation Aborted. Error Occured");
+  }
 
 
   //Subscriber Part of the Node
