c2b5c83b555c84d5ae140884aaa1af6696f044ee
Hotchnerz <radam.troy@gmail.com>
2022-03-14 10:05:03 -0400

    Test conversion from quaternion to Euler

--- src/execute_milestones/src/boxLocalization.cpp
+++ src/execute_milestones/src/boxLocalization.cpp
@@ -1,8 +1,9 @@
 #include <ros/ros.h>
-#include <tf/transform_listener.h>
+#include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Twist.h>
-//#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "box_localization");
@@ -38,17 +39,21 @@ int main(int argc, char** argv){
       continue;
     } 
     
-    double x, y, z, q_x, q_y, q_z, q_w, test = 0;
+    double x, y, z, q_x, q_y, q_z, q_w, r, p, y = 0;  
 
     transformStamped.transform.translation.x = x;
     transformStamped.transform.translation.y = y;
     transformStamped.transform.translation.z = z;
-    
 
     transformStamped.transform.rotation.x = q_x;
     transformStamped.transform.rotation.y = q_y;
     transformStamped.transform.rotation.z = q_z;
     transformStamped.transform.rotation.w = q_w;
+
+    tf2::Quaternion q(q_x, q_y, q_z, q_w);
+    tf2::Matrix3x3 euler(q);
+
+    euler.getRPY(r, p, y);
 
   
     int time = 3;
@@ -60,11 +65,11 @@ int main(int argc, char** argv){
     ROS_INFO_STREAM_THROTTLE(time, "y: " << y);
     ROS_INFO_STREAM_THROTTLE(time, "z: " << z);
     ROS_INFO_THROTTLE(time, " ");
-    ROS_INFO_THROTTLE(time, "Quaternion Coordinates:");
-    ROS_INFO_STREAM_THROTTLE(time, "x: " << q_x);
-    ROS_INFO_STREAM_THROTTLE(time, "y: " << q_y);
-    ROS_INFO_STREAM_THROTTLE(time, "z: " << q_z);
-    ROS_INFO_STREAM_THROTTLE(time, "z: " << q_w);
+    ROS_INFO_THROTTLE(time, "Orientation in Fixed Euler Angles:");
+    ROS_INFO_STREAM_THROTTLE(time, "Roll: " << r);
+    ROS_INFO_STREAM_THROTTLE(time, "Pitch: " << p);
+    ROS_INFO_STREAM_THROTTLE(time, "Yaw: " << y);
+    //ROS_INFO_STREAM_THROTTLE(time, "z: " << q_w);
 
 
     /*geometry_msgs::Twist vel_msg;
