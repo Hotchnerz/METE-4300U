44c6265434c9ee5ae5b17a058707c095621f0009
Hotchnerz <radam.troy@gmail.com>
2022-03-14 22:31:37 -0400

    Fixed transform listener by moving varibles outside of while loop.

--- src/execute_milestones/src/boxLocalization.cpp
+++ src/execute_milestones/src/boxLocalization.cpp
@@ -1,9 +1,10 @@
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Twist.h>
-#include <tf2/LinearMath/Matrix3x3.h>
-#include <tf2/LinearMath/Quaternion.h>
+#include <std_msgs/Float64.h>
+//#include <tf2/LinearMath/Quaternion.h>
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "box_localization");
@@ -19,19 +20,42 @@ int main(int argc, char** argv){
   turtle.request.theta = 0;
   turtle.request.name = "turtle2";
   spawner.call(turtle);
-
   ros::Publisher turtle_vel =
     node.advertise<geometry_msgs::Twist>("turtle2/cmd_vel", 10);*/
 
   tf2_ros::Buffer tfBuffer;
   tf2_ros::TransformListener tfListener(tfBuffer);
 
-  //ros::Rate rate(10.0);
+  tf2_ros::Buffer tfBuffer2;
+  tf2_ros::TransformListener tfListener2(tfBuffer2);
+  //geometry_msgs::PoseStamped poseStamped;
+  geometry_msgs::TransformStamped transformStamped;
+  geometry_msgs::TransformStamped transformStamped2;
+  double x, y, z, q_x, q_y, q_z, q_w, x2, y2, z2;
+
+
+  ros::Rate rate(10.0);
   while (node.ok()){
-    geometry_msgs::TransformStamped transformStamped;
+
     try{
-      transformStamped = tfBuffer.lookupTransform("map", "marker_id5",
-                               ros::Time(0));
+      transformStamped = tfBuffer.lookupTransform("map", "marker_id3", ros::Time(0)); 
+    x = transformStamped.transform.translation.x;
+    y = transformStamped.transform.translation.y;
+    z = transformStamped.transform.translation.z;
+
+    }
+    catch (tf2::TransformException &ex) {
+      ROS_WARN("%s",ex.what());
+      ros::Duration(1.0).sleep();
+      continue;
+    } 
+
+    try{
+    transformStamped2 = tfBuffer2.lookupTransform("map", "marker_id4", ros::Time(0)); 
+
+    x2 = transformStamped2.transform.translation.x;
+    y2 = transformStamped2.transform.translation.y;
+    z2 = transformStamped2.transform.translation.z;
     }
     catch (tf2::TransformException &ex) {
       ROS_WARN("%s",ex.what());
@@ -39,24 +63,30 @@ int main(int argc, char** argv){
       continue;
     } 
     
-    double x, y, z, q_x, q_y, q_z, q_w, roll, pitch, yaw = 0;  
 
-    transformStamped.transform.translation.x = x;
-    transformStamped.transform.translation.y = y;
-    transformStamped.transform.translation.z = z;
+    //std::cout << transformStamped << std::endl;
+
+
+
+    std::cout << "x: " << x << std::endl;
+    std::cout << "y: " << y << std::endl;
+    std::cout << "z: " << z << std::endl;
+
+    std::cout << "-------------------------------" << std::endl;
+
+    std::cout << "x: " << x2 << std::endl;
+    std::cout << "y: " << y2 << std::endl;
+    std::cout << "z: " << z2 << std::endl;
+    
+
 
     transformStamped.transform.rotation.x = q_x;
     transformStamped.transform.rotation.y = q_y;
     transformStamped.transform.rotation.z = q_z;
     transformStamped.transform.rotation.w = q_w;
 
-    tf2::Quaternion q(q_x, q_y, q_z, q_w);
-    tf2::Matrix3x3 euler(q);
-
-    euler.getRPY(roll, pitch, yaw);
-
   
-    int time = 3;
+    /*int time = 3;
     //std::cout << "Box 1 WRT to Map Origin:" << std::endl;
     ROS_INFO_THROTTLE(time, "Box 1 WRT to Map Origin:");
     ROS_INFO_THROTTLE(time, "-----------------------------------");
@@ -65,20 +95,21 @@ int main(int argc, char** argv){
     ROS_INFO_STREAM_THROTTLE(time, "y: " << y);
     ROS_INFO_STREAM_THROTTLE(time, "z: " << z);
     ROS_INFO_THROTTLE(time, " ");
-    ROS_INFO_THROTTLE(time, "Orientation in Fixed Euler Angles:");
-    ROS_INFO_STREAM_THROTTLE(time, "Roll: " << roll);
-    ROS_INFO_STREAM_THROTTLE(time, "Pitch: " << pitch);
-    ROS_INFO_STREAM_THROTTLE(time, "Yaw: " << yaw);
-    //ROS_INFO_STREAM_THROTTLE(time, "z: " << q_w);
+    ROS_INFO_THROTTLE(time, "Quaternion Coordinates:");
+    ROS_INFO_STREAM_THROTTLE(time, "x: " << q_x);
+    ROS_INFO_STREAM_THROTTLE(time, "y: " << q_y);
+    ROS_INFO_STREAM_THROTTLE(time, "z: " << q_z);
+    ROS_INFO_STREAM_THROTTLE(time, "z: " << q_w);*/
 
 
     /*geometry_msgs::Twist vel_msg;
-
     vel_msg.angular.z = 4.0 * atan2(transformStamped.transform.translation.y,
                                     transformStamped.transform.translation.x);
     vel_msg.linear.x = 0.5 * sqrt(pow(transformStamped.transform.translation.x, 2) +
                                   pow(transformStamped.transform.translation.y, 2));
     turtle_vel.publish(vel_msg);*/
   }
+  
+  rate.sleep();
   return 0;
 }
